f25303a29ef6cfa7096efb022de41df3a222bc10
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-03-24 23:38:45 -0600

    Add methods get for id, put,delete , get all and create product

--- src/main/java/com/learning_api/demo/product_rest/ProductsRest.java
+++ src/main/java/com/learning_api/demo/product_rest/ProductsRest.java
@@ -2,14 +2,13 @@ package com.learning_api.demo.product_rest;
 
 import com.learning_api.demo.dao.ProductsDAO;
 import com.learning_api.demo.entitys.Product;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController // Es un restapi
 @RequestMapping("products") //url en la que se expondran los servivios
@@ -23,6 +22,43 @@ public class ProductsRest {
         List<Product>products = productsDAO.findAll();
         return ResponseEntity.ok(products);
 
+    }
+
+    @RequestMapping(value = "{productId}") // products/{productId} -> /products/1
+    public ResponseEntity<Product> getProductById(@PathVariable("productId") Long productId){
+        Optional<Product> optionalProduct = productsDAO.findById(productId); // protege de un valor nulo optional
+        if (optionalProduct.isPresent()){
+            return ResponseEntity.ok(optionalProduct.get());
+        } else {
+            return ResponseEntity.noContent().build(); // Regresa que no encuentra nada
+        }
+
+    }
+
+    @PostMapping // /products(POST)
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product newProduct = productsDAO.save(product);
+        return ResponseEntity.ok(newProduct);
+    }
+
+    @DeleteMapping(value = "{productId}") // /products(DELETE)
+    public ResponseEntity<Void> deleteProduct(@PathVariable("productId") Long productId){
+        productsDAO.deleteById(productId);
+        return ResponseEntity.ok(null);
+    }
+
+
+    @PutMapping
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product){
+        Optional<Product> optionalProduct = productsDAO.findById(product.getId()); // protege de un valor nulo optional
+        if (optionalProduct.isPresent()){
+            Product updateProduct = optionalProduct.get();
+            updateProduct.setName(product.getName());
+            productsDAO.save(updateProduct);
+            return ResponseEntity.ok(updateProduct);
+        } else {
+            return ResponseEntity. notFound().build(); // Regresa que no encuentra nada
+        }
     }
 
 
